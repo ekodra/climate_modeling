8d3df7f174be0c08b022152b87a51372d689a6bf
Evan Kodra 2 <evan.kodra@gmail.com>
2019-02-28 10:46:51 -0500

    started adding handling for daily gcm preprocessing

--- lib/gcm_preprocessing.R
+++ lib/gcm_preprocessing.R
@@ -15,11 +15,49 @@ library(tidyr)
 risqGrids::connect_s3()
 export_bucket <- "risqinc"
 
-# test
 # hu4 <- sf::st_read("~/Downloads/wbdhu4_a_us_september2018.gdb/")
 
 
-# MAIN FUNCTIONS ------------------------------------------------------
+
+# SOME HANDY LEAP YEAR HANDLING DATA ------------------------------------------------
+leap_years <- 
+  c(1704, 1708, 1712, 1716, 1720, 1724, 1728, 
+  1732, 1736, 1740, 1744, 1748, 1752, 1756, 
+  1760, 1764, 1768, 1772, 1776, 1780, 1784, 
+  1788, 1792, 1796, 1804, 1808, 1812, 1816, 
+  1820, 1824, 1828, 1832, 1836, 1840, 1844, 
+  1848, 1852, 1856, 1860, 1864, 1868, 1872, 
+  1876, 1880, 1884, 1888, 1892, 1896, 1904, 
+  1908, 1912, 1916, 1920, 1924, 1928, 1932, 
+  1936, 1940, 1944, 1948, 1952, 1956, 1960, 
+  1964, 1968, 1972, 1976, 1980, 1984, 1988, 
+  1992, 1996, 2000, 2004, 2008, 2012, 2016, 
+  2020, 2024, 2028, 2032, 2036, 2040, 2044, 
+  2048, 2052, 2056, 2060, 2064, 2068, 2072, 
+  2076, 2080, 2084, 2088, 2092, 2096, 2104, 
+  2108, 2112, 2116, 2120, 2124, 2128, 2132, 
+  2136, 2140, 2144, 2148, 2152, 2156, 2160, 
+  2164, 2168, 2172, 2176, 2180, 2184, 2188, 
+  2192, 2196)
+
+month_df <- tibble(Month = 1:12, 
+                   Days = c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31))
+
+leap_month_df <- tibble(Month = 1:12, 
+                        Days = c(31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31))
+
+regular_month_seq <- purrr::map2(.x = month_df$Month, .y = month_df$Days,
+                                 .f = ~ rep(.x, .y)) %>% 
+  unlist() %>%
+  c()
+
+leap_month_seq <- purrr::map2(.x = leap_month_df$Month, .y = leap_month_df$Days,
+                              .f = ~ rep(.x, .y)) %>% 
+  unlist() %>%
+  c()
+
+
+# MAIN FUNCTIIONS ------
 preprocess_gcm <- function(polygon_sf = hu4, 
                            nc_key = s3_cmip5_historical_monthly_temperature$Key[1], 
                            resolution = "monthly", 
@@ -45,6 +83,17 @@ preprocess_gcm <- function(polygon_sf = hu4,
   lat <- ncvar_get(nc = ncf, varid = "lat")
   time <- ncvar_get(nc = ncf, varid = "time")
   
+  
+  meta_fields <- nc_key %>%
+    stringr::str_split(pattern = "/") %>% 
+    unlist() %>%
+    .[length(.)] %>%
+    stringr::str_split(pattern = "_") %>%
+    unlist()
+  
+  gcm <- meta_fields[3]
+  ic <- meta_fields[5]
+  
   Yr_bounds <- nc_key %>%
     stringr::str_split(pattern = "/") %>% 
     unlist() %>%
@@ -58,6 +107,10 @@ preprocess_gcm <- function(polygon_sf = hu4,
     unlist() %>% 
     as.numeric()
   
+  if(grepl(pattern = "HadGEM", x = nc_key)){
+    Yr_bounds[1] <- Yr_bounds[1] + 1
+  }
+  
   if(resolution == "monthly"){
     Year <- purrr::map(.x = Yr_bounds[1]:Yr_bounds[2], .f = ~ rep(.x, 12)) %>% 
       unlist() %>%
@@ -65,6 +118,34 @@ preprocess_gcm <- function(polygon_sf = hu4,
     
     Month <- rep(1:12, length(unique(Year)))
   }
+  
+  
+  if(resolution == "daily"){
+    
+    yr_df <- tibble(Year = Yr_bounds[1]:Yr_bounds[2], Len = 365)
+    
+    if(ncf$dim$time$calendar == "proleptic_gregorian"){
+      ## leap years -- need to handle
+      
+      yr_df$Len[yr_df$Year %in% leap_years] <- 366
+      
+      Year <- purrr::map2(.x = yr_df$Year, .y = yr_df$Len,
+                          .f = ~ rep(.x, .y)) %>% 
+        unlist() %>%
+        c()
+      
+      Month <- NULL
+      for(y in Yr_bounds[1]:Yr_bounds[2]){
+        Next <- regular_month_seq
+        if(y %in% leap_years){
+          Next <- leap_month_seq
+        }
+        Month <- c(Month, Next)
+      }
+      
+    }
+  }
+  
   
   # for each polygon, execute a cropping.
   ncCrop <- function(single_poly = polygon_sf[152,]){
@@ -147,30 +228,38 @@ preprocess_gcm <- function(polygon_sf = hu4,
   
   ts_mat <- as_tibble(purrr::reduce(.x = ts_list, .f = cbind))
   
-  ts_mat <- bind_cols(tibble(Year=Year, Month=Month), ts_mat)
+  ts_mat <- dplyr::bind_cols(tibble(Year=Year, Month=Month), ts_mat)
   
   colnames(ts_mat)[3:ncol(ts_mat)] <- as.character(polygon_sf[[spatial_defn_field]])
   
   ts_df <- tidyr::gather(ts_mat, key = "RegionName", value = climate_variable, 3:ncol(ts_mat))
-  
-  
-  meta_fields <- nc_key %>%
-    stringr::str_split(pattern = "/") %>% 
-    unlist() %>%
-    .[length(.)] %>%
-    stringr::str_split(pattern = "_") %>%
-    unlist()
-  
-  gcm <- meta_fields[3]
-  ic <- meta_fields[5]
   
   output <- list(gcm = gcm, ic = ic, resolution = resolution, ts_df = ts_df)
   
   return(output)
 }
 
+safely_preprocess_gcm <- purrr::safely(preprocess_gcm)
 
 
+
+# TESTING SECTION FOR DAILY DATA ------------------------------------------
+
+s3_cmip5_historical_daily_temperature <- aws.s3::get_bucket(
+  bucket = export_bucket,
+  prefix = "raw_data/CMIP5/daily/historical",
+  max = Inf
+) %>%
+  purrr::map(.f = ~`class<-`(.x, "list")) %>%
+  dplyr::bind_rows() %>%
+  dplyr::filter(grepl(pattern = "\\/tas_", Key))
+
+polygon_sf = hu4;
+nc_key = s3_cmip5_historical_daily_temperature$Key[1]; 
+resolution = "daily"; 
+scenario = "historical"; 
+climate_variable = "tas"; 
+spatial_defn_field = "NAME"
 
 
 
